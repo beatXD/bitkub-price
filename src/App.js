7674229a1f984f2e82c262dce6af09d9d231b70e
beattt <panupongl3@hotmail.com>
2021-04-22 18:00:48 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import logo from './logo.svg'
 import './App.css'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-
 function App() {
-  const [state, setstate] = useState()
+  const [bitkub, setBitkub] = useState()
+  const [binance, setBinance] = useState()
 
   useEffect(() => {
-    const fetchPrice = async () => {
-      const { data } = await axios.get('https://api.bitkub.com/api/market/ticker', {
+    const fetchBitkub = async () => {
+      const bitkub = await axios.get('https://api.bitkub.com/api/market/ticker', {
         headers: { 'Cache-Control': 'no-cache' },
       })
-      setstate(data)
+      setBitkub(bitkub.data)
     }
 
-    let increment = setInterval(() => fetchPrice(), 2000)
-    return () => clearInterval(increment)
-  }, [state])
+    const fetchBinance = async () => {
+      const binance = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
+      console.log('binance', binance.data)
+      setBinance(binance.data)
+    }
 
-  console.log('state', state)
+    let increment = setInterval(() => {
+      fetchBinance()
+      fetchBitkub()
+    }, 2000)
+    return () => clearInterval(increment)
+  }, [bitkub])
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Edit and save to reload.</p>
         <div>
-          {!state ? (
+          {!bitkub || !binance ? (
             <div> loading ..</div>
           ) : (
             <div>
-              <p> BTC PRICE : {state.THB_BTC.last.toFixed(5)} </p>
-              <p> BNB PRICE : {state.THB_BNB.last.toFixed(5)} </p>
+              <div>
+                <p> BITKUB PRICE </p>
+                <p> ------------ </p>
+                <p> USDT : ฿{bitkub.THB_USDT.last.toFixed(2)} </p>
+                <p> BTC : ฿{bitkub.THB_BTC.last.toFixed(2)} </p>
+                <p> LTC : ฿{bitkub.THB_LTC.last.toFixed(2)} </p>
+                <p> BNB : ฿{bitkub.THB_BNB.last.toFixed(2)} </p>
+                <p> DOGE : ฿{bitkub.THB_DOGE.last.toFixed(2)} </p>
+                <p> ADA : ฿{bitkub.THB_ADA.last.toFixed(2)} </p>
+                <p> ------------ </p>
+              </div>
+
+              <div>
+                <p> BINANCE PRICE </p>
+                <p> ------------ </p>
+                {/* <p> USDT : ฿{bitkub.THB_USDT.last.toFixed(2)} </p> */}
+                <p> BTC : ฿{binance.price} </p>
+                {/* <p> LTC : ฿{bitkub.THB_LTC.last.toFixed(2)} </p> */}
+                {/* <p> BNB : ฿{bitkub.THB_BNB.last.toFixed(2)} </p> */}
+                {/* <p> DOGE : ฿{bitkub.THB_DOGE.last.toFixed(2)} </p> */}
+                {/* <p> ADA : ฿{bitkub.THB_ADA.last.toFixed(2)} </p> */}
+                <p> ------------ </p>
+              </div>
             </div>
           )}
         </div>
