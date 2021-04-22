c838507da8e53e42c7af881e11005156628c6a28
beattt <panupongl3@hotmail.com>
2021-04-22 15:28:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [state, setstate] = useState()
+
+  useEffect(() => {
+    const fetchPrice = async () => {
+      const { data } = await axios.get('https://api.bitkub.com/api/market/ticker', {
+        headers: { 'Cache-Control': 'no-cache' },
+      })
+      setstate(data)
+    }
+
+    let increment = setInterval(() => fetchPrice(), 2000)
+    return () => clearInterval(increment)
+  }, [state])
+
+  console.log('state', state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Edit and save to reload.</p>
+        <div>
+          {!state ? (
+            <div> loading ..</div>
+          ) : (
+            <div>
+              <p> BTC PRICE : {state.THB_BTC.last.toFixed(5)} </p>
+              <p> BNB PRICE : {state.THB_BNB.last.toFixed(5)} </p>
+            </div>
+          )}
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
